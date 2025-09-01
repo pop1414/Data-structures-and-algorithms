564f5953a4cdaa55d3c1808b0fe5a3f5c684179b
Ayanami <940524396@qq.com>
2025-09-01 11:05:34 +0800

    Leetcode151

---
+++ String/LeetCode151/Reverse Words in a String.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int slow = 0;
+        for(int fast = 0; fast < s.size(); fast++){
+            if (s[fast] != ' '){
+                if (slow != 0){
+                    s[slow++] = ' ';
+                }
+                while(s[fast] != ' ' && fast < s.size()){
+                    s[slow++] = s[fast++];
+                }
+            }
+        }
+        s.resize(slow);
+
+        reverse(s.begin(), s.end());
+
+        for (int j = 0; j < slow; j++){
+            if(s[j] != ' '){
+                int left, right;
+                left = right = j;
+                while(s[right] != ' ' && right < slow){
+                    right++;
+                }
+                reverse(s.begin() + left, s.begin() + right);
+                j = right;
+            }
+        }
+        return s;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<pair<string,string>> tests = {
+        {"  hello world  ", "world hello"},
+        {"a   b   c", "c b a"},
+        {"   hello   ", "hello"},
+        {"     ", ""},
+        {"I love   C++  and   algorithms", "algorithms and C++ love I"},
+        {"a b c d", "d c b a"}
+    };
+
+    for (int i = 0; i < tests.size(); i++) {
+        string input = tests[i].first;
+        string expected = tests[i].second;
+        string output = sol.reverseWords(input);
+        cout << "样例 " << i+1 << ":\n";
+        cout << " 输入:    \"" << tests[i].first << "\"\n";
+        cout << " 输出:    \"" << output << "\"\n";
+        cout << " 期望:    \"" << expected << "\"\n";
+        cout << (output == expected ? " ✅ 正确" : " ❌ 错误") << "\n\n";
+    }
+    return 0;
+}
