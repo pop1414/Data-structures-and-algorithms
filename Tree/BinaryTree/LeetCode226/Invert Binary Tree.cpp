bcddefd557e254c28922c0a01fb0e7b8e73e25ca
Ayanami <940524396@qq.com>
2025-09-16 15:53:43 +0800

    LeetCode226

---
+++ Tree/BinaryTree/LeetCode226/Invert Binary Tree.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (!root)
+        {
+            return root;
+        }
+        traverse(root);
+        return root;
+    }
+    void traverse(TreeNode *root)
+    {
+        if (!root)
+        {
+            return;
+        }
+
+        traverse(root->left);
+        traverse(root->right);
+
+        TreeNode *temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+    }
+};
