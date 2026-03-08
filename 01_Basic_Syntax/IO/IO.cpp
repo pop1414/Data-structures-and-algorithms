a28bbb92809820f0cdbc4489e9e09173a9f2c27a
Ayanami <940524396@qq.com>
2026-03-08 10:53:33 +0800

    新增stdin的标准输流

--- 01_Basic_Syntax/IO/IO.cpp
+++ 01_Basic_Syntax/IO/IO.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
-void redirect_input(){
+// <iostream>
+void redirect_input()
+{
     ifstream fin("./test_case");
     cin.rdbuf(fin.rdbuf());
 }
 
-     
 int main()
 {
 
@@ -20,3 +21,44 @@ int main()
 
     return 0;
 }
+
+// <stdio>
+// void redirect_input()
+// {
+//     FILE *fin = freopen("./test_case", "r", stdin);
+
+//     if (fin == NULL)
+//     {
+//         fprintf(stderr, "错误：test_case 文件不存在或路径错误！\n");
+//         exit(1); // 打开失败直接退出程序
+//     }
+// }
+
+// int main()
+// {
+
+//     redirect_input();
+
+//     int n;
+//     scanf("%d", &n);
+
+//     int step = 0;
+
+//     while (n != 1)
+//     {
+//         if (n % 2 == 0)
+//         {
+//             n = n / 2;
+//             step++;
+//         }
+//         else
+//         {
+//             n = (3 * n + 1) / 2;
+//             step++;
+//         }
+//     }
+
+//     printf("%d", step);
+
+//     return 0;
+// }
