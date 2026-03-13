9cab397a28c774322a15c1ba9bd3c4ff6560b0cb
Ayanami <940524396@qq.com>
2026-03-13 16:30:06 +0800

    LeetCode307区域和检索 - 数组可修改（线段树）

---
+++ 08_Extend_OJ/01_LeetCode/LeetCode_307/leetcode_307.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+struct Node
+{
+    int val = 0;
+    int leftChildIdx = -1;
+    int rightChildIdx = -1;
+};
+
+class DynamicSegmentTree
+{
+private:
+    vector<Node> tree;
+    function<int(int, int)> merger;
+    long long maxRange;
+
+    int defaultVal;
+
+    int allocateNode()
+    {
+        Node newNode;
+        newNode.val = defaultVal;
+        tree.push_back(newNode);
+        return tree.size() - 1;
+    }
+
+    void update(long long l, long long r, int rootIndex, long long index, int val)
+    {
+        // 找到了叶子节点，赋值
+        if (l == r)
+        {
+            tree[rootIndex].val = val;
+            return;
+        }
+
+        long long mid = l + (r - l) / 2;
+        if (index <= mid)
+        {
+            if (tree[rootIndex].leftChildIdx == -1)
+            {
+                tree[rootIndex].leftChildIdx = allocateNode();
+            }
+            update(l, mid, tree[rootIndex].leftChildIdx, index, val);
+        }
+        else
+        {
+            if (tree[rootIndex].rightChildIdx == -1)
+            {
+                tree[rootIndex].rightChildIdx = allocateNode();
+            }
+            update(mid + 1, r, tree[rootIndex].rightChildIdx, index, val);
+        }
+
+        int leftVal = (tree[rootIndex].leftChildIdx != -1) ? tree[tree[rootIndex].leftChildIdx].val : defaultVal;
+        int rightVal = (tree[rootIndex].rightChildIdx != -1) ? tree[tree[rootIndex].rightChildIdx].val : defaultVal;
+
+        // 其余不是叶子节点的都是通过merger函数来赋值
+        tree[rootIndex].val = merger(leftVal, rightVal);
+    }
+
+    int query(long long l, long long r, int rootIndex, long long qL, long long qR)
+    {
+        if (rootIndex == -1)
+        {
+            return defaultVal;
+        }
+
+        if (qL == l && qR == r)
+        {
+            return tree[rootIndex].val;
+        }
+
+        long long mid = l + (r - l) / 2;
+
+        // qL与qR的范围还是完整的，mid在区间外
+        if (qR <= mid)
+        {
+            return query(l, mid, tree[rootIndex].leftChildIdx, qL, qR);
+        }
+        else if (qL > mid)
+        {
+            return query(mid + 1, r, tree[rootIndex].rightChildIdx, qL, qR);
+        }
+        // 横跨最主要的区别就是qL与qR的范围变化了（被mid切成了两部分）
+        else
+        {
+            int leftVal = query(l, mid, tree[rootIndex].leftChildIdx, qL, mid);
+            int rightVal = query(mid + 1, r, tree[rootIndex].rightChildIdx, mid + 1, qR);
+            return merger(leftVal, rightVal);
+        }
+    }
+
+public:
+    DynamicSegmentTree(long long max_range, function<int(int, int)> mergeFunc, int def_val) : maxRange(max_range), merger(mergeFunc), defaultVal(def_val)
+    {
+        tree.reserve(100000);
+        // 先创建一个根节点
+        allocateNode();
+    }
+
+    void update(long long index, int val)
+    {
+        update(0, maxRange, 0, index, val);
+    }
+
+    int query(long long qL, long long qR)
+    {
+        return query(0, maxRange, 0, qL, qR);
+    }
+};
+
+class NumArray
+{
+public:
+    NumArray(vector<int> &nums) : dst(nums.size() - 1, [](int a, int b)
+                                      { return a + b; }, 0)
+    {
+
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            dst.update(i, nums[i]);
+        }
+    }
+
+    void update(int index, int val)
+    {
+        dst.update(index, val);
+    }
+
+    int sumRange(int left, int right)
+    {
+        return dst.query(left, right);
+    }
+
+private:
+    DynamicSegmentTree dst;
+};
+
+int main()
+{
+    return 0;
+}
